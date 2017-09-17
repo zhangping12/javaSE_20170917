e5bbc001dd02e5c0eaac5c50ba2d0bad2ceb645c
zhangping12 <634001184@qq.com>
2017-09-17 17:00:34 +0800

    update

---
+++ src/main/java/ConditionalOperator.java
@@ -0,0 +1,11 @@
+//条件运算符
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 10;
+
+        int z = (x>y) ? x + y : x - y;//三目
+
+        System.out.println(z);
+    }
+}
---
+++ src/main/java/MiscOperators.java
@@ -0,0 +1,12 @@
+//其他运算符
+public class MiscOperators {
+    public static void main(String[] args) {
+      //  char c = 100;
+      //  int x = c;
+        int x = -100;
+        char c = (char)x;//取值范围,小范围转换成大范围的
+
+        System.out.println(x);
+
+    }
+}
