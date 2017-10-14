ac7b4ea55077e1116efeb7db792e5648b46bfd63
zhangping12 <634001184@qq.com>
2017-10-14 16:58:26 +0800

    update

--- src/main/java/cn/edu/tsinghua/javase/oop/ParameterPass.java
+++ src/main/java/cn/edu/tsinghua/javase/oop/ParameterPass.java
@@ -48,7 +48,7 @@ public class ParameterPass {
              String immutable
              primitive Wrapper
      */
-    private int i;
+    /*private int i;
 
     public ParameterPass(int i) {//有参构造方法
         this.i = i;
@@ -62,17 +62,36 @@ public class ParameterPass {
         this.i = i;
     }
     //静态方法
-    private static void method(ParameterPass parameterPass) {
+    private static void method(ParameterPass parameterPass) {//括号里面必须定义参数的类型，这个是引用类型
         System.out.println(parameterPass.getI()); // 0
         parameterPass.setI(1);
         System.out.println(parameterPass.getI()); // 1
     }
 
     public static void main(String[] args) {
-        ParameterPass parameterPass = new ParameterPass(0);
+        ParameterPass parameterPass = new ParameterPass(0);//引用有参构造方法
         System.out.println(parameterPass.getI()); // 0
         method(parameterPass);
         System.out.println(parameterPass.getI()); // 1
+    }*/
+
+    /**
+     * 注意：String类型以及基本数据类型的封装类是特例（还是值传递）
+           String immutable（不可改变的）
+           primitive Wrapper
+     * @param i
+     */
+    private static void method(String i) {
+       System.out.println(i); // 你好，
+       i += "中国";
+       System.out.println(i); // 你好，中国
+   }
+
+    public static void main(String[] args) {
+        String i = "你好，";
+        System.out.println(i); // 你好，
+        method(i);
+        System.out.println(i); //  你好，中国
     }
 
 }
