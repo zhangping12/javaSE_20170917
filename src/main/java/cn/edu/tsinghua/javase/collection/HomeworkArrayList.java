4270e63c1801e734de6fbab83910f39b8f4f23c1
zhangping12 <634001184@qq.com>
2017-10-17 23:10:28 +0800

    update

---
+++ src/main/java/cn/edu/tsinghua/javase/collection/HomeworkArrayList.java
@@ -0,0 +1,18 @@
+package cn.edu.tsinghua.javase.collection;
+
+import java.util.ArrayList;
+
+public class HomeworkArrayList {
+    // 求得 ArrayList 对象的容量？ 三大特性
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("hi");
+        strings.add("hello");
+
+        System.out.println(strings.size()); // 2
+
+        // Object[] elementData;
+        // length -> capacity;
+    }
+}
---
+++ src/main/java/cn/edu/tsinghua/javase/collection/OverrideTest.java
@@ -0,0 +1,17 @@
+package cn.edu.tsinghua.javase.collection;
+
+public class OverrideTest extends Object {
+
+
+    public static void main(String[] args) {
+        OverrideTest overrideTest = new OverrideTest();
+        System.out.println(overrideTest.toString()); // 隐式
+    }
+
+    @Override
+    public String toString() {
+        return "zhangsan";
+    }
+
+    // Object hashCode() equals() toString()
+}
