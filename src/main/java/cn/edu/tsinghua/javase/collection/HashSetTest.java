b47871a3daef10e6f02eb8db93256578caa37b61
zhangping12 <634001184@qq.com>
2017-10-18 23:15:49 +0800

    update

---
+++ src/main/java/cn/edu/tsinghua/javase/collection/HashSetTest.java
@@ -0,0 +1,19 @@
+package cn.edu.tsinghua.javase.collection;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+    public static void main(String[] args) {
+        HashSet<String> strings = new HashSet<>();
+        for (int i = 0; i < 100; i++) {
+            strings.add(i + "");
+        }
+        System.out.println(strings.size());
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+//        System.out.println(strings.get());
+    }
+}
---
+++ src/main/java/cn/edu/tsinghua/javase/collection/LinkedHashSetTest.java
@@ -0,0 +1,19 @@
+package cn.edu.tsinghua.javase.collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        LinkedHashSet<String> strings = new LinkedHashSet<>();
+        for (int i = 0; i < 100; i++) {
+            strings.add(i + "");
+        }
+
+        System.out.println(strings.size());
+//        System.out.println(strings.get);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
